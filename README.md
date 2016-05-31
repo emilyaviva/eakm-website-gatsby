16bd5d8956091496c90debabac5e49ea4538f603
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-05-30 22:43:36 -0700

    upgrade gatsby, use kitchen sink starter

---
+++ README.md
@@ -0,0 +1,23 @@
+# EAKM Website in Gatsby
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
+
+My professional website, on [emilyaviva.github.io/eakm-website-gatsby](GitHub Pages) until I get it moved somewhere else. Or not.
+
+Using [Gatsby](https://github.com/gatsbyjs/gatsby), based on the ["kitchen sink" default starter](https://github.com/gatsbyjs/gatsby-starter-default) by Kyle Mathews, licensed under the MIT license.
+
+## Running in development
+If Gatsby is installed globally:
+
+```
+npm install
+gatsby develop
+```
+
+If Gatsby is not installed globally:
+
+```
+npm install
+./node_modules/.bin/gatsby develop
+```
+
+Navigate your web browser to http://localhost:8000.
---
+++ pages/_template.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Container } from 'react-responsive-grid'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import Headroom from 'react-headroom'
+
+import { rhythm } from 'utils/typography'
+
+module.exports = React.createClass({
+  propTypes () {
+    return {
+      children: React.PropTypes.any
+    }
+  },
+  render () {
+    return (
+      <div>
+        <Headroom
+          wrapperStyle={{
+            marginBottom: rhythm(1)
+          }}
+        >
+          <Container
+            style={{
+              maxWidth: 960,
+              paddingTop: 0,
+              padding: `${rhythm(1)} ${rhythm(1 / 2)}`
+            }}
+          >
+            <Link to={prefixLink('/')}>
+              <h1>Emily Aviva Kapor-Mater</h1>
+              <h2>Full-Stack Software Developer</h2>
+            </Link>
+          </Container>
+        </Headroom>
+        <Container
+          style={{
+            maxWidth: 960,
+            padding: `${rhythm(1)} ${rhythm(1 / 2)}`,
+            paddingTop: 0
+          }}
+        >
+          {this.props.children}
+        </Container>
+      </div>
+    )
+  }
+})
