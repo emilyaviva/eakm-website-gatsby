61f999195b7be8b7da61e72b5ef1a6322cdee6c4
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-06-04 21:53:37 -0400

    add Header component; jsx updates

---
+++ shared/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import Navbar from './Navbar'
+
+export default class Header extends React.Component {
+  render () {
+    return (
+      <header>
+        <Link to={prefixLink('/')}>
+          <h1>Emily Aviva Kapor-Mater</h1>
+          <h2>Full-Stack Software Developer</h2>
+        </Link>
+        <Navbar />
+      </header>
+    )
+  }
+}
--- shared/PortfolioItem.js
+++ shared/PortfolioItem.js
@@ -4,9 +4,9 @@ export default class PortfolioItem extends React.Component {
   render () {
     return (
       <section className='portfolioItem'>
-        <a href={`${this.props.href}`}>
+        <a href={this.props.href}>
           <h3>{this.props.name}</h3>
-          <img src={`${this.props.imageSrc}`} alt={`${this.props.imageAlt}`} />
+          <img src={this.props.imageSrc} alt={this.props.imageAlt} />
         </a>
         <p>{this.props.desc}</p>
         <div style={{clear: 'both'}}></div>
