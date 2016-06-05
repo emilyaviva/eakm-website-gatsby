d98fe0194f4863169577570739bd8e2420eeece6
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-06-04 23:03:15 -0400

    removed react component dependencies

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import { Container } from 'react-responsive-grid'
-import { Link } from 'react-router'
-import { prefixLink } from 'gatsby-helpers'
-import Headroom from 'react-headroom'
-import Navbar from '../shared/Navbar'
+import Header from '../shared/Header'
 import Footer from '../shared/Footer'
-
-import { rhythm } from 'utils/typography'
 
 module.exports = React.createClass({
   propTypes () {
@@ -17,29 +11,8 @@ module.exports = React.createClass({
   render () {
     return (
       <div className='page-wrap'>
-        <Headroom wrapperStyle={{marginBottom: rhythm(1)}}>
-          <Container
-            style={{
-              maxWidth: 960,
-              paddingTop: 0,
-              padding: `${rhythm(3 / 4)} ${rhythm(1 / 2)}`
-            }}
-          >
-            <Link to={prefixLink('/')}>
-              <h1>Emily Aviva Kapor-Mater</h1>
-              <h2>Full-Stack Software Developer</h2>
-            </Link>
-            <Navbar />
-          </Container>
-        </Headroom>
-        <Container
-          style={{
-            maxWidth: 960,
-            padding: `${rhythm(1)} ${rhythm(1 / 2)} 0 ${rhythm(1 / 2)}}`
-          }}
-        >
-          {this.props.children}
-        </Container>
+        <Header />
+        {this.props.children}
         <Footer />
       </div>
     )
